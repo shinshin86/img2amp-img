7ce8b11ee369499e3e2c4c24f8e3144c017f2d5c
shinshin86 <beagles1986@gmail.com>
2021-03-07 22:09:38 +0900

    Support local image file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+const ps = require('process');
+const { promisify } = require('util');
+const sizeOf = promisify(require('image-size'));
 const requestImageSize = require('request-image-size');
 
 interface ImageObj {
@@ -6,13 +9,27 @@ interface ImageObj {
 }
 
 const convertToAmpImg = async (imageObj: ImageObj): Promise<string> => {
-  const response = await requestImageSize(imageObj.url);
+  const dimensions = await requestImageSize(imageObj.url);
 
   return `<amp-img
   alt="${imageObj.alt}"
   src="${imageObj.url}"
-  width="${response.width}"
-  height="${response.height}"
+  width="${dimensions.width}"
+  height="${dimensions.height}"
+  layout="responsive"
+></amp-img>`;
+};
+
+const convertToAmpImgFromLocalImage = async (
+  imageObj: ImageObj
+): Promise<string> => {
+  const dimensions = await sizeOf(`${ps.cwd()}/${imageObj.url}`);
+
+  return `<amp-img
+  alt="${imageObj.alt}"
+  src="${imageObj.url}"
+  width="${dimensions.width}"
+  height="${dimensions.height}"
   layout="responsive"
 ></amp-img>`;
 };
@@ -22,6 +39,10 @@ module.exports = async (imgTag: string): Promise<string> => {
     ? // @ts-ignore
       imgTag.match(/src=["|'](.*?)["|']/)[1]
     : '';
+
+  if (!url) {
+    throw new Error('Error: URL of image is not set in img tag');
+  }
 
   const alt = imgTag.match(/alt=["|'](.*?)["|']/)
     ? // @ts-ignore
@@ -33,6 +54,9 @@ module.exports = async (imgTag: string): Promise<string> => {
     alt,
   };
 
-  const ampImageTag = await convertToAmpImg(imageObj);
+  const ampImageTag =
+    url.indexOf('http') === 0
+      ? await convertToAmpImg(imageObj)
+      : await convertToAmpImgFromLocalImage(imageObj);
   return ampImageTag;
 };
